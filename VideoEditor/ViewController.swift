06100cec89b92bd4082aba61371a8306f0c26a29
muhdmirzamz <muhdmirzamz@gmail.com>
2018-04-10 22:16:01 +0800

    Multiple bugs involving existing files and not being able to merge videos

--- VideoEditor/ViewController.swift
+++ VideoEditor/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 import MobileCoreServices
 import AVFoundation
 import AVKit
+import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	var takingVideo = false
+	var loadingAsset = false
+	var firstAssetLoaded = false
+	
+	var firstAsset: AVAsset?
+	var secondAsset: AVAsset?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -51,8 +57,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		self.present(imagePicker, animated: true, completion: nil)
 	}
 	
+	@IBAction func loadFirstAsset() {
+		self.loadingAsset = true
+		
+		let imagePicker = UIImagePickerController()
+		imagePicker.allowsEditing = true
+		imagePicker.delegate = self
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.mediaTypes = [kUTTypeMovie as String]
+		
+		self.present(imagePicker, animated: true, completion: nil)
+	}
+	
+	@IBAction func loadSecondAsset() {
+		let imagePicker = UIImagePickerController()
+		imagePicker.allowsEditing = true
+		imagePicker.delegate = self
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.mediaTypes = [kUTTypeMovie as String]
+		
+		self.present(imagePicker, animated: true, completion: nil)
+	}
+	
 	public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-		if self.takingVideo {
+		if self.loadingAsset {
+			let mediaInfo = info[UIImagePickerControllerMediaType] as! String
+			
+			if self.firstAssetLoaded {
+				if mediaInfo == kUTTypeMovie as String {
+					self.secondAsset = AVAsset.init(url: info[UIImagePickerControllerMediaURL] as! URL)
+					
+					if let firstAsset = self.firstAsset, let secondAsset = self.secondAsset {
+						let alertController = UIAlertController.init(title: "Successfully loaded both assets", message: "", preferredStyle: .alert)
+						let okAction = UIAlertAction.init(title: "OK", style: .default, handler: nil)
+						
+						alertController.addAction(okAction)
+						
+						self.dismiss(animated: true) {
+							self.present(alertController, animated: true, completion: nil)
+						}
+					}
+				}
+			} else {
+				if mediaInfo == kUTTypeMovie as String {
+					self.firstAsset = AVAsset.init(url: info[UIImagePickerControllerMediaURL] as! URL)
+					
+					self.firstAssetLoaded = true
+					
+					self.dismiss(animated: true, completion: nil)
+				}
+			}
+		} else if self.takingVideo {
 			let mediaURL = info[UIImagePickerControllerMediaURL] as! URL
 			
 			self.dismiss(animated: true) {
@@ -65,7 +120,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 					alertController.addAction(okAction)
 					
 					self.present(alertController, animated: true, completion: nil)
-					
 				}
 			}
 		} else {
@@ -88,6 +142,90 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	
 	public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 		self.dismiss(animated: true, completion: nil)
+	}
+	
+	@IBAction func merge() {
+		let mixComposition = AVMutableComposition()
+		
+		let firstTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid))
+		do {
+			try firstTrack?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, (self.firstAsset?.duration)!), of: (firstAsset?.tracks(withMediaType: .video)[0])!, at: kCMTimeZero)
+		} catch {
+			print("Failed to load first track")
+		}
+		
+		let secondTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid))
+		do {
+			try secondTrack?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, (self.secondAsset?.duration)!), of: (secondAsset?.tracks(withMediaType: .video)[0])!, at: (firstAsset?.duration)!)
+		} catch {
+			print("Failed to load first track")
+		}
+		
+		let docDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+		let savePath = (docDirectory as NSString).appending("/mergevideo.mov")
+		let url = NSURL.fileURL(withPath: savePath)
+		
+		print("URL: \(url)")
+		
+		let exporter = AVAssetExportSession.init(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality)
+		exporter?.outputURL = url
+		exporter?.outputFileType = AVFileType.mov
+		exporter?.exportAsynchronously(completionHandler: {
+			DispatchQueue.main.async {
+				self.exportDidFinish(session: exporter!)
+			}
+		})
+	}
+	
+	func exportDidFinish(session: AVAssetExportSession) {
+		if session.status == .completed {
+			let outputURL = session.outputURL
+			
+			if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum((outputURL?.path)!) {
+				let alertController = UIAlertController.init(title: "Successfully saved", message: "", preferredStyle: .alert)
+				let okAction = UIAlertAction.init(title: "Ok", style: .default) { (action) in
+					UISaveVideoAtPathToSavedPhotosAlbum((outputURL?.path)!, nil, nil, nil)
+				}
+				
+				alertController.addAction(okAction)
+				
+				self.present(alertController, animated: true) {
+					UISaveVideoAtPathToSavedPhotosAlbum((outputURL?.path)!, nil, nil, nil)
+				}
+			}
+		}
+		
+		if session.status == .failed {
+			let outputURL = session.outputURL
+			
+			let fileManager = FileManager.default
+			
+			if fileManager.fileExists(atPath: (outputURL?.path)!) {
+				do {
+					print("Here")
+					try fileManager.removeItem(atPath: (outputURL?.path)!)
+					
+					let outputURL = session.outputURL
+					
+					if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum((outputURL?.path)!) {
+						let alertController = UIAlertController.init(title: "Successfully saved", message: "", preferredStyle: .alert)
+						let okAction = UIAlertAction.init(title: "Ok", style: .default) { (action) in
+							UISaveVideoAtPathToSavedPhotosAlbum((outputURL?.path)!, nil, nil, nil)
+						}
+						
+						alertController.addAction(okAction)
+						
+						self.present(alertController, animated: true) {
+							UISaveVideoAtPathToSavedPhotosAlbum((outputURL?.path)!, nil, nil, nil)
+						}
+					}
+				} catch {
+					print("Failed to remove file")
+				}
+			}
+			
+			print("Export error: \(session.error)")
+		}
 	}
 }
 
