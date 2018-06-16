7231c66ee22513c997a6d134ebc5bac148d0a2d6
muhdmirzamz <muhdmirzamz@gmail.com>
2018-06-16 12:10:01 +0800

    Added initial code to test scrubbing on image previews in timeline

--- VideoEditor/ImageCollectionViewCell.swift
+++ VideoEditor/ImageCollectionViewCell.swift
@@ -11,4 +11,22 @@ import UIKit
 class ImageCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
 	var assetURL: URL?
+	
+//	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//		let touchLocation = touches.first?.location(in: superview)
+//
+//		print("diff: \(touchLocation)")
+//	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let touchLocation = touches.first?.location(in: self)
+		
+		if self.frame.contains(touchLocation!) {
+			print("Touch location: \((touchLocation?.x)!)")
+		}
+	}
+	
+	//	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+	//		<#code#>
+	//	}
 }
